24ae9f5c05dfd8c1301b6ced16ea46d96196257a
Hubert Miernik <hubert.miernik@wp.pl>
2019-09-14 16:24:41 +0200

    Update Const.java

--- app/src/main/java/com/example/touristrouteplanner/Const.java
+++ app/src/main/java/com/example/touristrouteplanner/Const.java
@@ -5,20 +5,12 @@ public class Const {
 
     static String HOST = "192.168.21.10";
 
-    public static final String URL_FILTER = "http://" + HOST + "/android_register_login/filter.php";
-    public static final String URL_GET_FILTER = "http://" + HOST + "/android_register_login/test2.php";
-    public static final String URL_REGIST = "http://" + HOST + "/android_register_login/register.php";
-    public static final String URL_ROUTES = "http://" + HOST + "/android_register_login/test.php";
-    public static final String URL_ADD_HISTORY = "http://" + HOST + "/android_register_login/history.php";
-    public static final String URL_LOGIN = "http://" + HOST + "/android_register_login/login.php";
-    public static final String URL_HISTORY_REMOVE = "http://" + HOST + "/android_register_login/history_remove.php";
-
-
-
-
-
-
-
-
+    public static final String URL_FILTER = "http://" + HOST + "/touristrouteplannerbackend/filter.php";
+    public static final String URL_GET_FILTER = "http://" + HOST + "/touristrouteplannerbackend/filter_get.php";
+    public static final String URL_REGIST = "http://" + HOST + "/touristrouteplannerbackend/register.php";
+    public static final String URL_ROUTES = "http://" + HOST + "/touristrouteplannerbackend/routes.php";
+    public static final String URL_ADD_HISTORY = "http://" + HOST + "/touristrouteplannerbackend/history.php";
+    public static final String URL_LOGIN = "http://" + HOST + "/touristrouteplannerbackend/login.php";
+    public static final String URL_HISTORY_REMOVE = "http://" + HOST + "/touristrouteplannerbackend/history_remove.php";
 
 }
